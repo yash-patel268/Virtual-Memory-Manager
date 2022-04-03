b33aa78abe7ce5fd49875f441788236008077a31
yash-patel268 <yashp2126@gmail.com>
2022-04-03 17:08:43 -0400

    Added findPage function

--- main.c
+++ main.c
@@ -6,13 +6,12 @@
 #define BUFFER_SIZE 256
 #define TLB_SIZE 16
 
-typedef struct {
-	unsigned char TLBpage[16];
-	unsigned char TLBframe[16];
+struct TLB{
+	unsigned char TLBpage[16], TLBframe[16];
 	int ind;
-} TLB;
+};
 
-int read(int pageNum, char *phyMem, int* offset){
+int readBin(int pageNum, char *PM, int* OF){
 	char buffer[BUFFER_SIZE];
 	memset(buffer, 0, sizeof(buffer));
 	FILE *fp;
@@ -32,9 +31,48 @@ int read(int pageNum, char *phyMem, int* offset){
     }
 	
 	for(int x=0; x<BUFFER_SIZE; x++){
-		*((phyMem + (*offset)*BUFFER_SIZE)+x) = buffer[x];
+		*((PM + (*OF)*BUFFER_SIZE)+x) = buffer[x];
 	}
 	
-	(*offset)++;
-	return (*offset)-1;
+	(*OF)++;
+	return (*OF)-1;
+}
+
+int findPage(int logicalAddr, char* PT, struct TLB *tlb,  char* PM, int* OF, int* pageFaults, int* TLBhits){
+	unsigned char mask = 0xFF, offset, pageNum;
+	bool TLBhit = false;
+	int frame = 0, value, newFrame = 0;
+
+	printf("Virtual adress: %d\t", logicalAddr);
+
+	pageNum = (logicalAddr >> 8) & mask;	
+
+	offset = logicalAddr & mask;
+
+	for (int x=0; x<TLB_SIZE; x++){
+		if(tlb->TLBpage[x] == pageNum){
+			frame = tlb->TLBframe[x];
+			TLBhit = true;
+			(*TLBhits)++;
+		}
+	}
+
+	if (TLBhit == false){
+		if (PT[pageNum] == -1){
+			newFrame = readFromDisk(pageNum, PM, OF);
+			PT[pageNum] = newFrame;
+			(*pageFaults)++;
+		}
+
+		frame = PT[pageNum];
+		tlb->TLBpage[tlb->ind] = pageNum;
+		tlb->TLBframe[tlb->ind] = PT[pageNum];
+		tlb->ind = (tlb->ind + 1) % TLB_SIZE;
+	}
+	
+	int index = ((unsigned char) frame * BUFFER_SIZE) + offset;
+	value = *(PM + index);
+
+	printf("Physical address: %d\t Value: %d\n",index, value);	
+	return 0;
 }
